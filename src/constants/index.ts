5f53bf2f8c68e878f45961db369a9e56d18f4028
geepytechnologies <geepytechnologies@gmail.com>
2024-06-12 05:42:32 +0100

    secret key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,6 @@ export class CONSTANTS {
   static API_KEY = import.meta.env.VITE_RAPID_API_KEY;
   static API_HOST = import.meta.env.VITE_RAPID_API_HOST;
   static SERVERURL = `${import.meta.env.VITE_SERVER_URL}/api`;
-  static secretkey = import.meta.env.VITE_ENCRYPT_KEY;
+  static secretkey = "hackerplayblob@123ad";
+  // static secretkey = import.meta.env.VITE_ENCRYPT_KEY;
 }
